da8b6acf04b4795cf980d458590420f2d1250207
Arunjai283 <arunjaiswar283@gmail.com>
2025-11-13 11:13:46 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Arun")
+
+const clientId ="1"
+let clientname = "Arun"
+let clientage = 18
+let clientisLoggedIn = false
+let clientstate ="Maharashtra"
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //  the type of undefined is same undefined 
+console.log(typeof null); // the type of null is object 
+console.table([clientname,clientage,clientisLoggedIn,clientstate])
